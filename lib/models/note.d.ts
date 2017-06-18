190da8035407f857f1eb093b66225bb706709736
shwld <shwld@outlook.com>
2017-06-18 18:50:11 +0900

    Delete dirname prop from Note

--- lib/models/note.d.ts
+++ lib/models/note.d.ts
@@ -2,7 +2,6 @@ export declare class Note {
     readonly type: string;
     id: string;
     path: string;
-    readonly dirPath: string;
     body: string;
     constructor(obj?: any);
 }
--- src/models/note.ts
+++ src/models/note.ts
@@ -1,10 +1,7 @@
-import { dirname } from 'path';
-
 export class Note {
   readonly type = 'note';
   id: string;
   path: string;
-  get dirPath(): string { return dirname(this.path); }
   body: string;
   constructor(obj: any = {}) {
     Object.assign(this, obj);
